63e9201f4421b237a787a21f977ac726b5b82954
tryjk <161914053+tryjk@users.noreply.github.com>
2025-12-19 11:46:36 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,12 +1,5 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 6.0"
-    }
-  }
-}
-
 provider "aws" {
   region = "ap-south-1"
+  access_key = "AKIA6MLNJZYE7WBZKQUZ"
+  secret_key = "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"
 }
